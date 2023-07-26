cb2f7567faef2754c1f508fd8b728ade81949e54
jeothankachan270898 <jeothankachan270898@gmail.com>
2023-07-26 11:43:48 +0530

    query add serching added

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.get('/todos', function (req, res) {
     var queryParams = req.query;
     var filterdTodos = todos;
     if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filterdTodos = _.where(filterdTodos, {completed: true});
+        filterdTodos = _.where(filterdTodos, { completed: true });
     } else if (queryParams.completed === 'false' && queryParams.hasOwnProperty('completed')) {
-        filterdTodos = _.where(filterdTodos, {completed: false});
+        filterdTodos = _.where(filterdTodos, { completed: false });
     }
 
-    if(queryParams.hasOwnProperty('q')&& queryParams.q.length> 0) {
-        filterdTodos = _.filter(filterdTodos, function(todo) {
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filterdTodos = _.filter(filterdTodos, function (todo) {
             return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
         })
     }
@@ -37,8 +37,8 @@ app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
 
     //using underscore's findWhere method to find the todo with the given id
-    var matchedTodo = _.findWhere(todos,{id: todoId});
-  
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -54,10 +54,10 @@ app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
     //using underscore for validation
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length=== 0) {
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
     }
-    
+
     body.description = body.description.trim();
 
     //adding the new todo to the array
@@ -67,11 +67,11 @@ app.post('/todos', function (req, res) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos,{id: todoId});
+    var matchedTodo = _.findWhere(todos, { id: todoId });
     if (!matchedTodo) {
-        return res.status(404).json({"error":'Todo not found'});
+        return res.status(404).json({ "error": 'Todo not found' });
     } else {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
@@ -82,33 +82,33 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos,{id: todoId});
+    var matchedTodo = _.findWhere(todos, { id: todoId });
     var body = _.pick(req.body, 'description', 'completed');
     var validAttributes = {};
 
-    if(!matchedTodo) {
+    if (!matchedTodo) {
         return res.status(400).send();
     }
-    
+
     if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
         validAttributes.completed = body.completed;
-    }else if (body.hasOwnProperty('completed')){
+    } else if (body.hasOwnProperty('completed')) {
         return res.status(400).send();
     }
 
     if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-            validAttributes.description = body.description;
-        }else if (body.hasOwnProperty('description')){
-            return res.status(400).send();
-        }
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
 
     // Here we have a valid todo to update
     _.extend(matchedTodo, validAttributes);
     res.json(matchedTodo);
-    
+
 
 });
 
 app.listen(PORT, function () {
-   console.log('Server running on port'+ PORT);
+    console.log('Server running on port' + PORT);
 });
