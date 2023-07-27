2940e7b683b751f8593de6130a89a1ba6b65d430
jeothankachan270898 <jeothankachan270898@gmail.com>
2023-07-27 18:21:54 +0530

    jWT authentication added

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.post('/users', function (req, res) {
 app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
     db.user.authenticate(body).then(function (user) {
-        res.json(user.toPublicJSON());
+        res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
     }, function (err) {
         res.status(400).json(err);
     })
